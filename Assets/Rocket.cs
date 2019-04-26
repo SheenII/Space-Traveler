26ed2a981dd88283de5b2cd052dfeb609e8e868a
DESKTOP-DBDP50N\ashee <asheen127@comcast.net>
2019-04-26 08:31:37 -0500

    Update to previouse commit. Found a problem with a curly brace that led to sound issues. Fixed rotation to be more centered and natural looking.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -9,6 +9,8 @@ public class Rocket : MonoBehaviour
     Rigidbody rigidBody; // Do it like this. Simply put, the function is Rigidbody and the variable is rigidBody. This simply tells the
                          // class to look in unity for rigidbody. It will use what is attached to this class.
     AudioSource audioSource;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,32 +21,47 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Rotate();
+        Thrust();
     }
 
-    private void ProcessInput()
+    private void Rotate()
     {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+
+        
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+       
+        if (Input.GetKey(KeyCode.A)) // Turn left
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D)) // Turn right
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+        rigidBody.freezeRotation = false;
+    }
+
+    private void Thrust()
+    {
+
         if (Input.GetKey(KeyCode.Space)) // You can thrust while rotating.
         {
-            rigidBody.AddRelativeForce(Vector3.up); //Vector3 gives you a X,Y,Z position. We use it to give position and then create
-                                                    //movement. See more here - https://docs.unity3d.com/ScriptReference/Vector3.html
+            print("Space bar pressed");
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust); //Vector3 gives you a X,Y,Z position. We use it to give position and then create
+                                                                 //movement. See more here - https://docs.unity3d.com/ScriptReference/Vector3.html
+            Debug.Log("Sound is not playing. Sound started");
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
             }
-            else
-            {
-                audioSource.Stop();
-            }
         }
-
-        if (Input.GetKey(KeyCode.A)) // Turn left
+        else
         {
-            transform.Rotate(Vector3.forward);
+            Debug.Log("Space Bar not pressed. Sound Stopped");
+            audioSource.Stop();
         }
-        else if (Input.GetKey(KeyCode.D)) // Turn right
-        {
-            transform.Rotate(-Vector3.forward);
-        }
+        
     }
 }
